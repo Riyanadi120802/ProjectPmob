29b9f81295821bd09b14879a80a9daa2deaec660
Danz28 <119670330+Danzz28@users.noreply.github.com>
2022-12-15 12:50:14 +0700

    Add files via upload

--- app/src/main/java/com/example/projectpmob/MainActivity.java
+++ app/src/main/java/com/example/projectpmob/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.projectpmob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        search = (Button) findViewById(R.id.searchButton);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent cari = new Intent(MainActivity.this, SearchActivity.class);
+                    startActivity(cari);
+                }
+        });
     }
 }
