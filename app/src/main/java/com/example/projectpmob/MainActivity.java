dfe837ea5049db1c9db911236c3b99c71592e20e
riyanadi120802 <riyanadi083@gmail.com>
2022-12-29 14:02:53 +0700

    SQLite Update2

--- app/src/main/java/com/example/projectpmob/MainActivity.java
+++ app/src/main/java/com/example/projectpmob/MainActivity.java
@@ -1,48 +1,59 @@
 package com.example.projectpmob;
-
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    public static ShowActivity main;
-    Button search, show;
-    Menu menu;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public CardView card1, card2, card3, card4;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHelper db = new DatabaseHelper(this);
-//        db.perbarui(db.getWritableDatabase());
-
-        search = (Button) findViewById(R.id.searchButton);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    Intent cari = new Intent(MainActivity.this, SearchActivity.class);
-                    startActivity(cari);
-                }
-        });
-
-        show = (Button) findViewById(R.id.ButtonShow);
-        show.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent pindah = new Intent(MainActivity.this, ShowActivity.class);
-                startActivity(pindah);
-            }
-        });
+        card1 = (CardView) findViewById(R.id.search_button);
+        card2 = (CardView) findViewById(R.id.show_button);
+        card3 = (CardView) findViewById(R.id.tambah_button);
+        card4 = (CardView) findViewById(R.id.help_button);
 
 
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.search_button :
+                i = new Intent(this, SearchActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.show_button:
+                i = new Intent(this, ShowActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.help_button:
+                i = new Intent(this, HelpActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.tambah_button:
+                i = new Intent(this, Buat_data.class);
+                startActivity(i);
+                break;
+        }
     }
 }
