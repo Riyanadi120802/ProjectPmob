ab766deb9d9988fea59784d0dca966cadfcdc27c
Ilham <93887615+ilhamsbi@users.noreply.github.com>
2023-01-05 13:35:21 +0700

    Update HelpActivity.java
    bantuan baru

--- app/src/main/java/com/example/projectpmob/HelpActivity.java
+++ app/src/main/java/com/example/projectpmob/HelpActivity.java
@@ -1,14 +1,25 @@
 package com.example.newpmob;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class HelpActivity extends AppCompatActivity {
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        btn3 = (Button) findViewById(R.id.button3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
